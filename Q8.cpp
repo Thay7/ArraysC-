3b8602b1910ffc9f5fe0e07807129ea75592e5da
Thay7 <thay3x@gmail.com>
2022-07-17 19:11:13 -0300

    up q8

--- Q8.cpp
+++ Q8.cpp
@@ -1,3 +1,5 @@
+// Faça um algoritmo para ler 15 números e armazenar em um vetor VET, verificar e escrever se existem números repetidos no vetor VET e em que posições se encontram
+
 #include <iostream>
 using namespace std;
 
@@ -5,35 +7,52 @@ int main()
 {
     int VET[15];
     int positions[15];
-    int n, j = 0, cont = 0;
+    int alreadyFound[15];
+    int number, j = 0, cont = 0, cont2 = 0;
 
     for (int i = 0; i < 15; i++)
     {
-        cout << "Digite um número: ";
-        cin >> n;
+        cout << "Digite um número para preencher o vetor: ";
+        cin >> number;
 
-        VET[i] = n;
+        VET[i] = number;
     }
-        for (int j = 0; j < 15; j++)
+    for (int j = 0; j < 15; j++)
+    {
+        cont = 0;
+        cont2 = 0;
+
+        for (int n = 0; n < 15; n++)
         {
-            if (n == VET[j])
-            {
-                cont++;
-            }
+            if (VET[j] == alreadyFound[n])
+                cont2++;
         }
+
+        if (cont2 == 0)
+        {
 
             for (int k = 0; k < 15; k++)
             {
-                if (cont > 1)
+                if (VET[k] == VET[j])
                 {
-                    positions[k] = k;
-
-                    cout << "O número " << n << " se repete " << cont << " vezes no vetor, nas posições " << positions[k];
-                }
-                else
-                {
-                    cout << "Não existem números repetidos no vetor!";
+                    positions[cont] = k;
+                    cont++;
                 }
             }
+
+            if (cont > 1)
+            {
+                alreadyFound[j] = VET[j];
+
+                cout << "O número " << VET[j] << " se repete " << cont << " vezes no vetor, nas posições ";
+
+                for (int l = 0; l < cont; l++)
+                {
+                    cout << positions[l] << ", ";
+                }
+
+                cout << "\n";
+            }
         }
-    
+    }
+}
