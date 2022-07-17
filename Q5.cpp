d5e0330726e9b3e0e0e30f0c9742ae68b7d7d2c9
Thay7 <thay3x@gmail.com>
2022-07-17 17:17:25 -0300

    up q5

--- Q5.cpp
+++ Q5.cpp
@@ -1,23 +1,41 @@
+// 05. Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    float media[10];
-    float nota = 0;
+    float V[10];
+    float nota1, nota2, nota3, nota4;
+    float soma, media, cont = 0;
 
-    for(int i = 0; i >= 10; i++)
+    for (int i = 0; i < 2; i++)
     {
-            cout << "Digite a primeira nota do " << i+1 << "º aluno: ";
-            cin >> nota; 
-            cout << "Digite a segunda nota do " << i+1 << "º aluno: ";
-            cin >> nota; 
-            cout << "Digite a terceira nota do " << i+1 << "º aluno: ";
-            cin >> nota; 
-            cout << "Digite a quarta nota do " << i+1 << "º aluno: ";
-            cin >> nota; 
-            
-                cout << endl; 
-    }
+        cout << "Digite a primeira nota do " << i + 1 << "º aluno: ";
+        cin >> nota1;
+        cout << "Digite a segunda nota do " << i + 1 << "º aluno: ";
+        cin >> nota2;
+        cout << "Digite a terceira nota do " << i + 1 << "º aluno: ";
+        cin >> nota3;
+        cout << "Digite a quarta nota do " << i + 1 << "º aluno: ";
+        cin >> nota4;
 
-}
+        cout << "\n";
+        
+        soma = nota1 + nota2 + nota3 + nota4;
+        media = soma / 4;
+
+        V[i] = media;
+    }
+        for (int j = 0; j < 2; j++)
+        {
+            if (V[j] >= 7)
+            { 
+                cont++;
+            }
+
+            cout << "\n";
+        }
+
+        cout << cont << " alunos tem média maior ou igual a 7.";
+    }
